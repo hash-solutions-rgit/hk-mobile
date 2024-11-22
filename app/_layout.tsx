6d3847738bad28225b17d6498f4a62203c87b96d
sjana7797 <sjana7797@gmail.com>
2024-11-23 00:03:44 +0530

    feat: adding first page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -14,12 +14,15 @@ import { useColorScheme } from "~mobile/hooks/useColorScheme";
 
 import "../global.css";
 import { Text } from "react-native";
+import {
+  SafeAreaFrameContext,
+  SafeAreaView,
+} from "react-native-safe-area-context";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
@@ -35,12 +38,12 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
+    <ThemeProvider value={DefaultTheme}>
+      <Stack
+        screenOptions={{
+          headerShown: false,
+        }}
+      />
     </ThemeProvider>
   );
 }
---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { SafeAreaView, Text } from "react-native";
+
+function Home() {
+  return (
+    <SafeAreaView className="bg-gray-50">
+      <Text>Hello world</Text>
+    </SafeAreaView>
+  );
+}
+
+export default Home;
